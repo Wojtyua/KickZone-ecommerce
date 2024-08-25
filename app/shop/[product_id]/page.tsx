0eb4f7c058c0bfca179fb0020c240856a3c4e7d5
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-08-25 12:56:32 +0200

    added: dynamic title for product page

--- app/shop/[product_id]/page.tsx
+++ app/shop/[product_id]/page.tsx
@@ -1,8 +1,16 @@
 import { getProductById } from "@/app/_lib/mongodb/productActions";
 
+export async function generateMetadata({
+  params,
+}: {
+  params: { product_id: string };
+}) {
+  const { data } = await getProductById(params.product_id);
+  return { title: `${data.product_model}` };
+}
+
 const Page = async ({ params }: { params: { product_id: string } }) => {
-  const productId = params.product_id;
-  const { data, message } = await getProductById(productId);
+  const { data, message } = await getProductById(params.product_id);
   return (
     <div>
       <span>{message}</span>
