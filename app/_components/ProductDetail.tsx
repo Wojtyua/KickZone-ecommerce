2af61392db71e38374fab0e4738261b4b266ebd0
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-08-26 20:32:26 +0200

    basic functionality for productDetail

--- app/_components/ProductDetail.tsx
+++ app/_components/ProductDetail.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { ProductType } from "@/app/_lib/mongodb/db.types";
+import { useState } from "react";
 import Image from "next/image";
-import React, { useState } from "react";
+import { ProductType } from "@/app/_lib/mongodb/db.types";
 
 type ProductDetailProps = {
   data: ProductType;
@@ -17,13 +17,19 @@ const ProductDetail: React.FC<ProductDetailProps> = ({
     setSelectedSize(size);
   };
 
+  const handleAddToCart = () => {
+    if (selectedSize) {
+      console.log(`Added ${product_model} to cart in size ${selectedSize}`);
+    }
+  };
+
   return (
     <div>
       <h1>{product_model}</h1>
       <p>{description}</p>
       <p>{`Price: $${price.toFixed(2)}`}</p>
 
-      <div className="product-images">
+      <div>
         {images.map((image, index) => (
           <Image
             width={200}
@@ -35,16 +41,16 @@ const ProductDetail: React.FC<ProductDetailProps> = ({
         ))}
       </div>
 
-      <div className="product-sizes">
+      <div>
         <h3>Select Size:</h3>
-        <div className="sizes">
+        <div className="flex gap-2">
           {variants.map((variant) => (
             <button
               key={variant.size}
               onClick={() => handleSizeChange(variant.size)}
               disabled={variant.quantity === 0}
-              className={`size-button ${
-                selectedSize === variant.size ? "selected" : ""
+              className={`size-12 border ${
+                selectedSize === variant.size ? "border-red-400" : ""
               }`}
             >
               {variant.size}
@@ -53,7 +59,11 @@ const ProductDetail: React.FC<ProductDetailProps> = ({
         </div>
       </div>
 
-      <button disabled={!selectedSize} className="add-to-cart-button">
+      <button
+        disabled={!selectedSize}
+        onClick={handleAddToCart}
+        className="add-to-cart-button"
+      >
         Add to Cart
       </button>
     </div>
